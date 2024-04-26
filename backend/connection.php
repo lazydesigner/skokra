7b5240653ef239e6a92b3c8dd3ee8f6a8406fe75
deepak <deepakbaradwaj933@gmail.com>
2024-04-26 16:49:27 +0530

    b

--- backend/connection.php
+++ backend/connection.php
@@ -47,7 +47,6 @@ class DatabaseConnection
                 $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-                echo 'CONNECTION IS MADE SECURELY';
             } catch (PDOException $e) {
                 echo "Error: " . $e->getMessage();
             }
