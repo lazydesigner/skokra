dbe2e867592a098940d85074008a2fac454ff096
deepak <deepakbaradwaj933@gmail.com>
2024-04-26 17:41:02 +0530

    b

--- image_processing/image-upload.php
+++ image_processing/image-upload.php
@@ -54,8 +54,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             imagedestroy($image);
             if (file_exists($destinationPath)) {
                 unlink($target . basename($_FILES['image']['name']));
-
-                $con = mysqli_connect('localhost', 'root', '', 'skokra');
+                if (isLocalhost()) {$con = mysqli_connect('localhost', 'root', '', 'skokra');}else{$con = mysqli_connect('localhost', 'u231955561_inskokra', 'Skokra@12com', 'u231955561_in_skokra');}
+                
                 if (!$con) {
                     die('failed to connect');
                 }
