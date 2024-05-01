00a2c9e8c7e7bc811bfe3a5a9cb1abf8081afc6f
deepak <deepakbaradwaj933@gmail.com>
2024-05-01 17:05:27 +0530

    b

--- account/dashboard/post-ad/post-insert-image.php
+++ account/dashboard/post-ad/post-insert-image.php
@@ -64,7 +64,7 @@ if (isset($stopthefurtherprocess)) {
         .preview-image-box-grid {
             display: grid;
             
-            grid-template-columns: repeat(auto-fit, minmax(20%, 1fr));
+            grid-template-columns: repeat(5, minmax(20%, 1fr));
             grid-auto-rows: 250px;
             justify-content: space-between;
             /* grid-template-rows: auto; */
