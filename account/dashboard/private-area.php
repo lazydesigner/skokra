fa28f72394af28c6a750f5a1667d418887c2a982
deepak <deepakbaradwaj933@gmail.com>
2024-05-03 00:43:44 +0530

    b

--- account/dashboard/private-area.php
+++ account/dashboard/private-area.php
@@ -5,7 +5,7 @@
             <div id="cloase-the-private-area">X</div>
         </div>
         <div class="private-area-body">
-            <div class="private-area-subheading">Hi! <?php if(isset($_SESSION['email'])){echo $_SESSION['email']; } ?></div>
+            <div class="private-area-subheading">Hey! <?php if(isset($_SESSION['email'])){echo $_SESSION['email']; } ?></div>
             <div class="private-area-action">
                 <div class="private-area-items">
                     <a href="<?=get_url()?>u/account/dashboard"><span><i class="ri-home-5-line"></i><br>
